35c0354a6d5c47120a4f8cd594fda187237337b5
Mohammed Faizan Ul Islam <mohammedfaizan3103@gmail.com>
2026-05-27 23:44:45 +0530

    Add files via upload

---
+++ spark_retail_with_instructions (1)/tumbling_window.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, to_timestamp, sum, window
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.appName("TumblingWindow").getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+
+schema = StructType([
+    StructField("InvoiceNo", StringType()),
+    StructField("StockCode", StringType()),
+    StructField("Description", StringType()),
+    StructField("Quantity", IntegerType()),
+    StructField("InvoiceDate", StringType()),
+    StructField("UnitPrice", DoubleType()),
+    StructField("CustomerID", StringType()),
+    StructField("Country", StringType())
+])
+
+df = spark.readStream.option("header", True).schema(schema).csv("retail_stream")
+
+df = df.withColumn("eventTime", to_timestamp(col("InvoiceDate")))
+df = df.withColumn("TotalPrice", col("Quantity") * col("UnitPrice"))
+
+result = df.withWatermark("eventTime", "1 day") \
+    .groupBy(window(col("eventTime"), "1 day"), col("Country")) \
+    .agg(sum("TotalPrice").alias("Revenue"))
+
+query = result.writeStream.outputMode("append").format("console").start()
+query.awaitTermination()
